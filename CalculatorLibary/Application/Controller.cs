bbb06aacb0a46e87368f5d492e805eb6baad61cd
Molex <simplesleeptimer@gmail.com>
2019-03-12 10:59:54 +0100

    Add more stuff

---
+++ CalculatorLibary/Application/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetLibrary.Application
+{
+    public class Controller
+    {
+        private Controller _instance;
+
+        public Controller Instance => _instance ?? (_instance = new Controller());
+
+        private Controller()
+        {
+
+        }
+    }
+}
