c0c0786425a02f0ec98d0c2bb96ce19c51164069
Molex <simplesleeptimer@gmail.com>
2019-03-14 10:47:53 +0100

    Add stuff to Controller

--- CalculatorLibary/Application/Controller.cs
+++ CalculatorLibary/Application/Controller.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BudgetLibrary.Domain;
+using BudgetLibrary.Model;
+using BudgetLibrary.Persistence;
+
 namespace BudgetLibrary.Application
 {
     public class Controller
@@ -15,6 +19,16 @@ namespace BudgetLibrary.Application
         private Controller()
         {
 
+        }
+
+        public IBudget GetBudget(int id)
+        {
+            return SQLConnector.Instance.GetBudget(id);
+        }
+
+        public List<IBudget> GetBudgets()
+        {
+            return new List<IBudget>(SQLConnector.Instance.GetBudgets());
         }
     }
 }
