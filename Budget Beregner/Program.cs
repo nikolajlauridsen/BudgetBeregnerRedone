a49a67e54ad8cb31253d6fb1d8da8b8036943b2c
Molex <simplesleeptimer@gmail.com>
2019-03-14 10:11:56 +0100

    Add empty model interfaces

--- Budget Beregner/Program.cs
+++ Budget Beregner/Program.cs
@@ -14,32 +14,32 @@ namespace Budget_Beregner
     {
         static void Main(string[] args)
         {
-            /*
+
             Program myProgram = new Program();
             myProgram.Run();
-            */
 
-            /*
+
+
             Budget budgetToSave = new Budget("Hopefully the final test budget");
             budgetToSave.AddIncome("SU", 10000, -1);
             budgetToSave.AddIncome("Løn", 20000, -1);
             budgetToSave.AddExpense("Husleje", 10000, -1);
             budgetToSave.AddExpense("Benzin", 8000, -1);
 
-            budgetToSave = SQLConnector.Instance.SaveBudget(budgetToSave.Name, budgetToSave.Incomes, budgetToSave.Expenses);
-            */
+            //budgetToSave = SQLConnector.Instance.SaveBudget(budgetToSave.Name, budgetToSave.Incomes, budgetToSave.Expenses);
 
-            Budget myBudget = SQLConnector.Instance.GetBudget(10);
-            Console.WriteLine($"Budget: {myBudget.Name}\nID: {myBudget.ID}");
 
-            Console.WriteLine("\nIncomes: ");
-            foreach(Income income in myBudget.Incomes) Console.WriteLine($"{income.Name}: {income.Amount}");
+            //Budget myBudget = SQLConnector.Instance.GetBudget(10);
+            //Console.WriteLine($"Budget: {myBudget.Name}\nID: {myBudget.ID}");
 
-            Console.WriteLine("\nExpenses: ");
-            foreach (Expense expense in myBudget.Expenses) Console.WriteLine($"{expense.Name}: {expense.Amount}");
+            //Console.WriteLine("\nIncomes: ");
+            //foreach(Income income in myBudget.Incomes) Console.WriteLine($"{income.Name}: {income.Amount}");
 
-            Console.WriteLine($"\nDisposable income: {myBudget.CalculateDisposableIncome()}");
-            Console.ReadKey(true);
+            //Console.WriteLine("\nExpenses: ");
+            //foreach (Expense expense in myBudget.Expenses) Console.WriteLine($"{expense.Name}: {expense.Amount}");
+
+            //Console.WriteLine($"\nDisposable income: {myBudget.CalculateDisposableIncome()}");
+            //Console.ReadKey(true);
         }
 
         void Run()
