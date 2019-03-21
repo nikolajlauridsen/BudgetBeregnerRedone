3788d462fde580dcfa90a406b0cd2cdf71eefc62
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2019-03-21 18:48:10 +0100

    Started implementing CreateBudget GUI is done

--- BudgetBeregnerGUI/CreateBudgetPage.xaml.cs
+++ BudgetBeregnerGUI/CreateBudgetPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BudgetLibrary.Application;
 
 namespace BudgetBeregnerGUI
 {
@@ -20,9 +21,38 @@ namespace BudgetBeregnerGUI
     /// </summary>
     public partial class CreateBudgetPage : Page
     {
+
         public CreateBudgetPage()
         {
             InitializeComponent();
+
         }
+
+        private void AddIncome_Click(object sender, RoutedEventArgs e)
+        {
+            IncomeList.Items.Add(new MyItems {Name = AddIncomeName.Text, Amount = AddIncomeAmount.Text});
+
+        }
+
+        private void AddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseList.Items.Add(new MyItems { Name = AddExpenseName.Text, Amount = AddExpenseAmount.Text });
+        }
+
+        private void AddBtnShowBudget_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class MyItems
+        {
+            public string Name;
+            public string Amount;
+        }
+
+
     }
+
+
+
 }
